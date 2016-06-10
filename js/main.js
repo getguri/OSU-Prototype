8be04fee19d7ce65662542969078a6c3d180262a
GetGuri <gurvinder.singh@srijan.in>
2016-06-10 16:22:34 +0530

    tutor

--- js/main.js
+++ js/main.js
@@ -1,73 +1,168 @@
-var app = angular.module('myApp', ['ngAnimate']);
+var app = angular.module('mgcrea.ngStrapDocs', ['ngAnimate', 'ngSanitize', 'mgcrea.ngStrap']);
 
-app.controller('myCtrl', function($scope) {
-	$scope.name = '';
-	$scope.profession = '';
-	$scope.counsel = '';
-	$scope.school = '';
-	$scope.career ='';
-	$scope.section='';
-	$scope.admissions = false;
-	$scope.careers = false;
+app.directive("btstAccordion", function () {
+    return {
+        restrict: "E",
+        transclude: true,
+        replace: true,
+        scope: {},
+        template:
+            "<div class='accordion' ng-transclude></div>",
+        link: function (scope, element, attrs) {
+
+            // give this element a unique id
+            var id = element.attr("id");
+            if (!id) {
+                id = "btst-acc" + scope.$id;
+                element.attr("id", id);
+            }
+
+            // set data-parent on accordion-toggle elements
+            var hdr1 = element.children().children().eq(0).children().eq(0);
+            var hdr2 = element.children().children().eq(2).children().eq(0);
+            var hdr3 = element.children().children().eq(4).children().eq(0);
+            
+                $(hdr1).attr("data-parent", "#" + id);
+                $(hdr1).attr("href", "#" + id + "collapse" + 0);
+            	
+                $(hdr2).attr("data-parent", "#" + id);
+                $(hdr2).attr("href", "#" + id + "collapse" + 1);
+         
+                $(hdr3).attr("data-parent", "#" + id);
+                $(hdr3).attr("href", "#" + id + "collapse" + 2);
+            
+            var body1 = element.children().children().eq(1);
+            var body2 = element.children().children().eq(3);
+            var body3 = element.children().children().eq(5);
+
+            $(body3).addClass("in"); // expand first pane
+                $(body1).attr("id", id + "collapse" + 0);
+            	$(body2).attr("id", id + "collapse" + 1);
+                $(body3).attr("id", id + "collapse" + 2); 
+        },
+        controller: function () {}
+    };
+}).
+directive('btstPane', function () {
+    return {
+        require: "^btstAccordion",
+        restrict: "E",
+        transclude: true,
+        replace: true,
+        scope: {
+            title: "@",
+            category: "=",
+            order: "="
+        },
+        template:
+            "<div class='accordion-group' >" +
+            "  <div class='accordion-heading'>" +
+            "    <i class='fa fa-plus-square-o accordion-toggle' data-toggle='collapse'>{{category.name}} -</i><i class='fa fa-edit'></i>" +
+            "  </div>" +
+            "<div class='accordion-body collapse'>" +
+            "  <div class='accordion-inner' ng-transclude></div>" +
+            "  </div>" +
+            "</div>",
+        link: function (scope, element, attrs) {
+            scope.$watch("title", function () {
+                // NOTE: this requires jQuery (jQLite won't do html)
+                var hdr = element.children().eq(0).children().eq(0);
+                hdr.html(scope.title);
+            });
+        }
+    };
 });
 
+app.controller('CounterController', function($scope) {
+
+  $scope.count = 0;
+  
+  $scope.increment = function() {
+    if ($scope.count >= 15 ) { return; }
+    $scope.count++;
+  };
+  $scope.decrement = function() {
+    if ($scope.count <= 0) { return; }
+    else $scope.count--;
+  };
+
+}).controller('TimepickerCtrl', function($scope) {
+  $scope.time = new Date(1970, 0, 1, 10, 30, 40);
+}).controller('myCtrl', function($scope) {
+	$scope.name = '';
+	$scope.career ='';
+	$scope.address='';
+	$scope.location = false;
+	$scope.course = false;
+    $scope.profession = '';
+    $scope.counsel = '';
+    $scope.school = '';
+    $scope.section='';
+    $scope.admissions = false;
+    $scope.careers = false;
+});
+
+
+
+
+
 $(document).ready(function(){
-	$('#firstStepRegistration input').on('blur change', function(){
+    $('#firstStepRegistration input').on('blur change', function(){
 
-		var yourFullNameText = $('#yourFullNameText');
-		var yourProfessionText = $('#yourProfessionText');
-		var yourStudentText = $('#yourStudentText');
-		var classSelect = $('#classSelect');
-		var sectionSelect = $('#sectionSelect');
-		var yourSchoolText = $('#yourSchoolText');
+        var yourFullNameText = $('#yourFullNameText');
+        var yourProfessionText = $('#yourProfessionText');
+        var yourStudentText = $('#yourStudentText');
+        var classSelect = $('#classSelect');
+        var sectionSelect = $('#sectionSelect');
+        var yourSchoolText = $('#yourSchoolText');
 
-		$("#stuname").val(yourStudentText);
+        $("#stuname").val(yourStudentText);
 
-		var count = 1;
-		var finalStatus = 0;
-		var ratio = 20;
-		var nameVal = yourFullNameText.val();
-		var professionVal = yourProfessionText.val();
-		var studentVal = yourStudentText.val();
-		var schoolVal = yourSchoolText.val();
-		if(typeof nameVal === 'string' && nameVal !== '') {
-			finalStatus += ratio;
-		}
-		if(typeof professionVal === 'string' && professionVal !== '') {
-				finalStatus += ratio;
-		}
-		if(typeof studentVal === 'string' && studentVal !== '') {
-				finalStatus += ratio;
-		}
-		if(typeof schoolVal === 'string' && schoolVal !== '') {
-				finalStatus += ratio;
-		}	
+        var count = 1;
+        var finalStatus = 0;
+        var ratio = 20;
+        var nameVal = yourFullNameText.val();
+        var professionVal = yourProfessionText.val();
+        var studentVal = yourStudentText.val();
+        var schoolVal = yourSchoolText.val();
+        if(typeof nameVal === 'string' && nameVal !== '') {
+            finalStatus += ratio;
+        }
+        if(typeof professionVal === 'string' && professionVal !== '') {
+                finalStatus += ratio;
+        }
+        if(typeof studentVal === 'string' && studentVal !== '') {
+                finalStatus += ratio;
+        }
+        if(typeof schoolVal === 'string' && schoolVal !== '') {
+                finalStatus += ratio;
+        }   
 
-		
-		var classVal= classSelect.val();
-		var sectionVal= sectionSelect.val();
-		if(typeof classVal === 'string' && classVal !== '' && typeof sectionVal === 'string' && sectionVal !== ''){
-			finalStatus  += ratio;
-		}
+        
+        var classVal= classSelect.val();
+        var sectionVal= sectionSelect.val();
+        if(typeof classVal === 'string' && classVal !== '' && typeof sectionVal === 'string' && sectionVal !== ''){
+            finalStatus  += ratio;
+        }
 
-		finalStatus = Math.round(finalStatus);
-	 	if(finalStatus > 0){
-	 		$('#firstStepProgressReg').addClass('progress-started');
-	 	} else {
-	 		$('#firstStepProgressReg').removeClass('progress-started');
-	 	}
+        finalStatus = Math.round(finalStatus);
+        if(finalStatus > 0){
+            $('#firstStepProgressReg').addClass('progress-started');
+        } else {
+            $('#firstStepProgressReg').removeClass('progress-started');
+        }
 
-	 	if(finalStatus == 100){
-	 		$('#firstStepProgressReg').addClass('progress-end');
-	 	} else {
-	 		$('#firstStepProgressReg').removeClass('progress-end');
-	 	}
-	 	
-	 	$('#firstStepProgressReg .progress').css('width', finalStatus + "%");
-	 });
+        if(finalStatus == 100){
+            $('#firstStepProgressReg').addClass('progress-end');
+        } else {
+            $('#firstStepProgressReg').removeClass('progress-end');
+        }
+        
+        $('#firstStepProgressReg .progress').css('width', finalStatus + "%");
+     });
 
-	/*Career you are interested in search field*/
-	$('#careersInterestedInAdded').on('click', '.close', function(){
+    /*Career you are interested in search field*/
+		$('#careersInterestedInAdded').on('click', '.close', function(){
  		$(this).parent().remove();
  	});
 
@@ -93,8 +188,6 @@ $(document).ready(function(){
 	};
 	$("#careersInterestedInText").easyAutocomplete(careerConsider);
 
-	/*soring*/
-
 	function updateCount(){
 		$('#careersInterestedInAdded > li').each(function(i, el){
 			$(this).find('.count').html(i+1);
@@ -112,7 +205,32 @@ $(document).ready(function(){
 		next.after(item);
 		updateCount();
 	});
+
+	/* Code for Star Rating */
+	$(".star-rating-radio input[type='radio']").on('change', function(){
+		$(this).parent().prevAll("label").addClass('active');
+		$(this).parent().addClass('active');
+		$(this).parent().nextAll("label").removeClass('active');
+	});
+		
+	/* click on edit button fields enable */
+
+	$('#starrating .edit').click(function(){
+		$(this).parents('.fields').find('input,textarea').attr('disabled', false);
+		$('#starrating').removeClass('disabled');
+	});
+
+	/* disabled button default */
+	$('#starrating span').click(function(){
+		if($('#starrating').hasClass('disabled')){
+			return false	
+		}
+	});
+
+	$(".thumb-rating-radio input[type='radio']").on('change', function(){
+		$(this).parent().addClass('active');
+		$(this).parent().next().addClass('active');
+		$(this).parent().prevAll().removeClass('active');
+
+	});	
 });
-
-
-
