e215830ff7f58c061aeabda87aa5434f6b56abba
GetGuri <gurvinder.singh@srijan.in>
2016-06-22 16:47:49 +0530

    new form

--- js/main.js
+++ js/main.js
@@ -299,35 +299,35 @@ $(document).ready(function(){
     min: 0,
     max: 660,
     step: 15,
-    values: [0, 360],
+    values: [0,660],
     slide: function (e, ui) {
-        var hours2 = Math.floor(ui.values[1] / 60);
-        var minutes2 = ui.values[1] - (hours2 * 60);
+        var hours1 = Math.floor(ui.values[0] / 60);
+        var minutes1 = ui.values[0] - (hours1 * 60);
 
-        if (hours2.length == 1) hours2 = '0' + hours2;
-        if (minutes2.length == 1) minutes2 = '0' + minutes2;
-        if (minutes2 == 0) minutes2 = '00';
-        if (hours2 >= 12) {
-            if (hours2 == 12) {
-                hours2 = hours2;
-                minutes2 = minutes2 ;
-            } else if (hours2 == 24) {
-                hours2 = 11;
-                minutes2 = "59 PM";
+        if (hours1.length == 1) hours1 = '0' + hours2;
+        if (minutes1.length == 1) minutes1 = '0' + minutes1;
+        if (minutes1 == 0) minutes1 = '00';
+        if (hours1 >= 12) {
+            if (hours1 == 12) {
+                hours1 = hours1;
+                minutes1 = minutes1 ;
+            } else if (hours1 == 24) {
+                hours1 = 11;
+                minutes1 = "59 PM";
             } else {
-                hours2 = hours2 - 12;
-                minutes2 = minutes2 ;
+                hours1 = hours1 - 12;
+                minutes1 = minutes1 ;
             }
         } else {
-            hours2 = hours2;
-            minutes2 = minutes2;
+            hours1 = hours1;
+            minutes1 = minutes1;
         }
 
         if ($("#morning").is(":checked")) {
-            $('.slider-time2').val(hours2 + ':' + minutes2+' AM');
+            $('.slider-time2').val(hours1 + ':' + minutes1+' AM');
         }
         else{
-            $('.slider-time2').val(hours2 + ':' + minutes2+' PM');
+            $('.slider-time2').val(hours1 + ':' + minutes1+' PM');
             if($('.slider-time2').val()=="0.00 PM"){
                 $('.slider-time2').val("12.00 PM");
             }
